0ef42492c5b3f3c78fc1a5e59d9e24f65cb414d8
Vicente Sanchez <etneciv223@gmail.com>
2017-03-05 03:45:26 +0100

    programa inicial

--- proyectfecha/src/proyectfecha/Fecha.java
+++ proyectfecha/src/proyectfecha/Fecha.java
@@ -10,7 +10,17 @@ public class Fecha {
 	private	int mes;
 	private int	anio;
 	
-	private int diasMes(){
+
+	public Fecha(int dia, int mes, int anio) {
+		this.dia = dia;
+		this.mes = mes;
+		this.anio = anio;
+		}
+	public boolean valida ( ) {
+		if (dia < 1 || dia > 31) return false;
+		if (mes < 1 || mes > 12) return false;
+		if (anio < 0) return false;
+		
 		// determinamos la cantidad de días del mes:
 		int diasMes = 0;
 		switch (mes) {
@@ -35,22 +45,9 @@ public class Fecha {
 					diasMes = 28;
 					break;
 				}
-		return diasMes;
-	}
-	
-	
-	public Fecha(int dia, int mes, int anio) {
-		this.dia = dia;
-		this.mes = mes;
-		this.anio = anio;
-		}
-	public boolean valida ( ) {
-		if (dia < 1 || dia > 31) return false;
-		if (mes < 1 || mes > 12) return false;
-		if (anio < 0) return false;
-		if (dia>diasMes()) return false;
-		else return true;
-		}
+				if (dia>diasMes) return false;
+				else return true;
+				}
 		
 		
 	public static void main(String[] args) throws NumberFormatException, IOException  {
